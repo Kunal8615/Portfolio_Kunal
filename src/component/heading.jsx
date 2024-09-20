e05e9c4ca64dbeb3831972c5b8707b919ce4239b
Kunal8615 <113933312+Kunal8615@users.noreply.github.com>
2024-09-20 18:50:15 +0530

    linked

--- src/component/heading.jsx
+++ src/component/heading.jsx
@@ -16,7 +16,7 @@ function Heading({prop}){
     </h1>
 </div>
 <p className=" p-2 mb-6 text-lg font-normal text-yellow-100 lg:text-xl sm:px-16 xl:px-48 ">B.Tech student with experience in JS, HTML, CSS, Graphic designing, gaming and programming languages like C and C++. Passionate about using technology to enhance user experience and solve real-world problems. Enthusiastic about exploring new technologies and ideas to stay ahead in the fast-paced tech industry.</p>
-<button type="button" className="mb-5 text-black font-medium bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 rounded-full text-lg px-6 py-2 text-center me-2 mb-2 dark:focus:ring-yellow-900">More</button>
+<button type="button" className="mb-5 text-black font-medium bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 rounded-full text-lg px-6 py-2 text-center me-2 mb-2 dark:focus:ring-yellow-900" href="https://www.linkedin.com/in/kunal-1a4983228/">More</button>
 </div>
        
 </>
