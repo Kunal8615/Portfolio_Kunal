9c7775d09b9675b8034b3cd7814dea62210029d6
Kunal8615 <113933312+Kunal8615@users.noreply.github.com>
2025-09-18 19:20:19 +0530

    fixed error

--- src/component/project/Project.jsx
+++ src/component/project/Project.jsx
@@ -1,56 +1,70 @@
-import React from 'react';
+import React from "react";
 import resume from "../../images/resume.png";
 import tubetweet from "../../images/tubetweet.png";
-import ProjectCard from './component';
-import datacloud from "../../images/datacloud.png"
-import scratchify from "../../images/Scratchify.png"
-import Social from '../social';
+import ProjectCard from "./component";
+import datacloud from "../../images/datacloud.png";
+import scratchify from "../../images/Scratchify.png";
+import Social from "../social";
 
-const techStack = `JavaScript 🖥️: For interactive functionality and DOM manipulation.\nHTML 📄: Provides structure and content.\nCSS 🎨: For styling and layout.\nPDF Export 📄: Allows PDF download of resumes.\nResponsive Design 📱: Ensures compatibility across devices.`;
+const techStack = `JavaScript 🖥️: For interactive functionality and DOM manipulation.
+HTML 📄: Provides structure and content.
+CSS 🎨: For styling and layout.
+PDF Export 📄: Allows PDF download of resumes.
+Responsive Design 📱: Ensures compatibility across devices.`;
 
 function Project() {
   return (
     <>
-      <ProjectCard 
-        title={"TubeTweet"} 
-        tech={`💻 Frontend: Vite 🔧Backend: Node.js, Express, JavaScript\n📊 Database: MongoDB\n🛠️ Testing: Postman`} // Use backticks for multiline strings
-        img={tubetweet} 
-        link={"tube-tweet-mu.vercel.app/"} 
-        des={"🚀 Introducing TubeTweet: Stream videos, tweet seamlessly, track subscribers, upload content, and create playlists. Built with Vite, Node.js, MongoDB. Explore it here:"} 
+      <ProjectCard
+        title={"TubeTweet"}
+        tech={`💻 Frontend: Vite 🔧Backend: Node.js, Express, JavaScript
+📊 Database: MongoDB
+🛠️ Testing: Postman`}
+        img={tubetweet}
+        link={"https://tube-tweet-mu.vercel.app/"}
+        des={
+          "🚀 Introducing TubeTweet: Stream videos, tweet seamlessly, track subscribers, upload content, and create playlists. Built with Vite, Node.js, MongoDB. Explore it here:"
+        }
       />
 
-          <ProjectCard 
-            title={"Scratchify"} 
-            tech={`💻 Frontend: Vite 🔧Backend: Node.js, Express, JavaScript\n📊 Database: MongoDB\n🛠️ Testing: Postman`}
-            img={scratchify} 
-            link={"https://scratchify-mocha.vercel.app/"} 
-            des={"About 🚀 Introducing Scratchify! 🎟️🔄 Ever had unused scratch cards from Google Pay, Paytm, or other platforms just sitting there? 🤔💸 With Scratchify, you can exchange them and find the most valuable ones! 💰✨ No more wasted rewards—get the best deals, trade wisely, and maximize your benefits! 🔄🔥 What do you think? Let me know! 💭👇"} 
-          />
+      <ProjectCard
+        title={"Scratchify"}
+        tech={`💻 Frontend: Vite 🔧Backend: Node.js, Express, JavaScript
+📊 Database: MongoDB
+🛠️ Testing: Postman`}
+        img={scratchify}
+        link={"https://scratchify-mocha.vercel.app/"}
+        des={
+          "About 🚀 Introducing Scratchify! 🎟️🔄 Ever had unused scratch cards from Google Pay, Paytm, or other platforms just sitting there? 🤔💸 With Scratchify, you can exchange them and find the most valuable ones! 💰✨ No more wasted rewards—get the best deals, trade wisely, and maximize your benefits! 🔄🔥 What do you think? Let me know! 💭👇"
+        }
+      />
 
-          <ProjectCard
-            title={"DataCloud"}
-            link={"https://data-cloud-rho.vercel.app/"}
-            img={datacloud}
-            des={"DataCloud ☁️ is a robust web application built with the MERN stack for seamless data storage and retrieval. It features secure upload and download capabilities with JWT token authentication 🔒. With an intuitive interface, DataCloud empowers users to easily manage their information 🎉"}
-            tech={`Technologies: Built with the MERN stack using React 🎨, Vite ⚡, Node.js 🖥️, and MongoDB 📊.
-Security: Features secure JWT authentication 🔒 `}
-          />
-          
-          <ProjectCard 
-            title={"Resume Builder"} 
-            link={"https://kunal8615.github.io/Resume_builder/"} 
-            img={resume} 
-            des={"The Resume Builder is an intuitive application designed to help users create professional resumes effortlessly. With its streamlined process, users can focus on showcasing their skills and experiences without any hassle."} 
-            tech={techStack} 
-          />
-        </div>
-      </section>
+      <ProjectCard
+        title={"DataCloud"}
+        link={"https://data-cloud-rho.vercel.app/"}
+        img={datacloud}
+        des={
+          "DataCloud ☁️ is a robust web application built with the MERN stack for seamless data storage and retrieval. It features secure upload and download capabilities with JWT token authentication 🔒. With an intuitive interface, DataCloud empowers users to easily manage their information 🎉"
+        }
+        tech={`Technologies: Built with the MERN stack using React 🎨, Vite ⚡, Node.js 🖥️, and MongoDB 📊.
+Security: Features secure JWT authentication 🔒`}
+      />
+
+      <ProjectCard
+        title={"Resume Builder"}
+        link={"https://kunal8615.github.io/Resume_builder/"}
+        img={resume}
+        des={
+          "The Resume Builder is an intuitive application designed to help users create professional resumes effortlessly. With its streamlined process, users can focus on showcasing their skills and experiences without any hassle."
+        }
+        tech={techStack}
+      />
 
       {/* Social Links */}
       <section className="py-8">
-        <Social/>
+        <Social />
       </section>
-    </div>
+    </>
   );
 }
 
